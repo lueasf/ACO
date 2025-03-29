7706b18dacd2f3640724fe9781b56955990799b4
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2025-03-29 18:08:48 +0100

    phase 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Geometric Optimization with CVXPY and ECOS
+
+This project demonstrates how to solve geometric optimization problems using convex programming.
+
+> It finds the extreme point of a convex shape (e.g., diamond) that maximizes projection along a specified direction.
+
+## ECOS
+ECOS (Embedded Conic Solver) is an open-source solver, designed for SOCP (Second Order Cone Programming) and SDP (Semidefinite Programming).
+
---
+++ main.py
@@ -0,0 +1,48 @@
+import cvxpy as cp
+import numpy as np
+import matplotlib.pyplot as plt
+
+def find_opti(direction):
+    point = cp.Variable(2) # create the var to optimize
+    contraintes = [cp.norm(point, 1) <= 1] # |x| + |y| <= 1
+    probleme = cp.Problem(cp.Maximize(direction @ point), contraintes)
+    probleme.solve(solver=cp.ECOS)
+    
+    if probleme.status != "optimal":
+        raise ValueError("No solution found")
+    
+    return point.value
+
+
+# Case 1 : lozenge
+direction = np.array([1.0, 0.0])  # ->
+point_opt = find_opti(direction)
+
+# ----------------------------------
+# Visualisation
+# ----------------------------------
+fig, ax = plt.subplots(figsize=(8, 8))
+
+
+theta = np.linspace(0, 2*np.pi, 100)
+x = np.cos(theta) / (np.abs(np.cos(theta)) + np.abs(np.sin(theta)))
+y = np.sin(theta) / (np.abs(np.cos(theta)) + np.abs(np.sin(theta)))
+ax.fill(x, y, alpha=0.3, label="lozenge |x| + |y| ≤ 1")
+
+ax.scatter(*point_opt, c='red', s=100, 
+           label=f'Optimal Point ({point_opt[0]:.2f}, {point_opt[1]:.2f})')
+
+# vector
+ax.quiver(0, 0, direction[0], direction[1], 
+          angles='xy', scale_units='xy', scale=1,
+          color='green', width=0.01, label='Direction')
+
+ax.set_title("Optimization on the lozenge")
+ax.set_xlim(-1.5, 1.5)
+ax.set_ylim(-1.5, 1.5)
+ax.grid(True)
+ax.axhline(0, color='black', linewidth=0.5)
+ax.axvline(0, color='black', linewidth=0.5)
+ax.set_aspect('equal')
+ax.legend()
+plt.show()
